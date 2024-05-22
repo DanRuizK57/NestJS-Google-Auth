d8ea348aca02e88b1dc87a0cdd671121891202b6
DanRuizK57 <d.ruiz03@ufromail.cl>
2024-05-22 12:29:57 -0400

    Instalación de dependencias + configuraciones del módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot()],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),
+    UserModule,
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.POSTGRESQL_HOST,
+        port: +process.env.POSTGRESQL_PORT,
+        username: process.env.POSTGRESQL_USERNAME,
+        password: process.env.POSTGRESQL_PASSWORD,
+        database: process.env.POSTGRESQL_DB_NAME,
+        // Ingresar entidades creadas
+        entities: [],
+        // No se debe usar en producción, se pueden perder datos.
+        synchronize: true,
+      })
+  ],
   controllers: [AppController],
   providers: [AppService, AuthService],
 })
